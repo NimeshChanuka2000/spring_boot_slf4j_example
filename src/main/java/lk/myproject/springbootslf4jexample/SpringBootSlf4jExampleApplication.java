778ec09a47dffbee82af8b86439631719527c2a2
NimeshChanuka2000 <nimeshchanu1234@gmail.com>
2024-04-08 23:51:48 +0530

    initial commit

---
+++ src/main/java/lk/myproject/springbootslf4jexample/SpringBootSlf4jExampleApplication.java
@@ -0,0 +1,13 @@
+package lk.myproject.springbootslf4jexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSlf4jExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSlf4jExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/myproject/springbootslf4jexample/SpringBootSlf4jExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.myproject.springbootslf4jexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSlf4jExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
